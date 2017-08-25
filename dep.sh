58bce6618f84bdff2ae8caa7bc20a6da75294f44
vzardlloo <vzardlloo@gmail.com>
2017-08-25 16:29:54 +0800

    默认提交，无特殊信息

--- dep.sh
+++ dep.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-echo "                           "
-echo "                           "
-echo "***************************"
-echo "*we will do a deploy soon!*"
-echo "***************************"
-echo "                           "
-echo "                           "
-echo "$@"
 
-git add "$1"
-git commit -m "Add solution"
-git push -u origin master
+echo "======开始提交======"
+
+if ["$1" == ""]
+then
+	set "默认提交，无特殊信息"
+
+fi
+
+git status
+git add .
+git commit -m $1
+git push
+echo "======提交成功=======”
+
+
